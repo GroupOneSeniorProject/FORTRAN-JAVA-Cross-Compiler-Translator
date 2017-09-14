e48fb5df0921e183e99ae5e0f3f39823e6519cc9
HWorrell <31424637+HWorrell@users.noreply.github.com>
2017-09-14 07:48:30 -0400

    Merging in what we were working on yesterday
    I've gone ahead and had the system load all keywords for Fortran up to and including for F95.  I just initialize the entire thing as a final array of strings, and then loaded it into the Linked List.

--- src/com/company/LexicalAnalyzer.java
+++ src/com/company/LexicalAnalyzer.java
@@ -9,11 +9,22 @@ public class LexicalAnalyzer
 
     public LexicalAnalyzer()
     {
-        keyWords.add("print");
-        keyWords.add("integer");
-        keyWords.add("program");
-        keyWords.add("end");
-        keyWords.add("real");
+        //this is an array of all keywords through Fortran 95
+        final String[] keywordsArray = {"real", "integer", "complex", "assign", "backspace", "block data", "call", "close", "common", "continue", "data", "dimension", "do", "else", "else if", "end","endfile","endif", "entry", "equivalence", "external", "format", "function", "goto", "if", "implicit", "inquire", "intrinsic", "open", "parameter", "pause", "print", "program", "read", "return", "rewind", "rewrite", "save", "stop", "subroutine", "then", "write", "allocatable", "allocate", "case", "contains", "cycle", "deallocate", "elsewhere", "exit", "include", "interface", "intent", "module", "namelist", "nullify", "only", "operator", "optional", "pointer", "private", "procedure", "public", "recursive", "result", "select", "sequence", "target", "use", "while", "where", "elemental", "forall", "pure"};
+        
+        //Use a loop to load all keywords into the list at once.
+        for(int i = 0; i < keywordsArray.length; i++)
+        {
+            
+            keyWords.add(keywordsArray[i]);
+            
+        }
+        
+        //keyWords.add("print");
+        //keyWords.add("integer");
+        //keyWords.add("program");
+        //keyWords.add("end");
+        //keyWords.add("real");
         openF95("test.f95"); //For IntelliJ, file must be in project root folder
         check();
     }
