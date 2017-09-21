e488e29afa4928fb1bdac505da8b39a5b1d98511
Johnathon Monson <johnathonmonson@gmail.com>
2017-09-21 11:58:30 -0400

    yes

---
+++ src/com/company/functions.java
@@ -0,0 +1,11 @@
+public class functions
+{
+    public functions()
+    {
+
+    }
+    String startprogram(String s){
+        String thisProgram = "public class " + s + "\ns{\n public static void main(String[] args)\n{\n";
+        return thisProgram;
+    }
+}
---
+++ src/com/company/main.java
@@ -0,0 +1,16 @@
+import java.lang.System.*;
+//import ;
+
+public class main 
+{
+
+    public static void main(String[] args)
+    {
+        GUI g = new GUI();
+
+        g.addAncestorListener();
+
+
+        //LexicalAnalyzer lex = new LexicalAnalyzer();
+    }
+}
