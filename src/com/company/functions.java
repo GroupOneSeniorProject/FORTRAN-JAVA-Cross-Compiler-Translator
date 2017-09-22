9e2bb38a7dcdae71d93f3781bf625d475d0c1f6c
HWorrell <31424637+HWorrell@users.noreply.github.com>
2017-09-22 12:04:00 -0400

    Updating Functions
    Added Logical Operators, if/else, and comparison operators

--- src/com/company/functions.java
+++ src/com/company/functions.java
@@ -12,4 +12,87 @@ public class functions
     {
         return "}\n";
     }
+    
+    String logical(String s)
+    {
+        String result = "";
+        if(s.equalsIgnoreCase(".eqv."))
+        {
+            return " == ";
+        }
+        else if(s.equalsIgnoreCase(".true."))
+        {
+            return " true";
+        }
+        else if(s.equalsIgnoreCase(".false."))
+        {
+            return " false";
+        }
+        else if(s.equalsIgnoreCase(".not."))
+        {
+            return "!";
+        }
+        else if(s.equalsIgnoreCase(".and."))
+        {
+            return " && ";
+        }
+        else if(s.equalsIgnoreCase(".or."))
+        {
+            return " || ";
+        }
+        else if(s.equalsIgnoreCase(".nequv."))
+        {
+            return " != ";
+        }
+
+        return "uncaught error";
+    }
+    String ifelse(String s)
+    {
+        if(s.equalsIgnoreCase("if"))
+        {
+            return "if(";
+        }
+        else if(s.equalsIgnoreCase("then"))
+        {
+            return ")\n{";
+        }
+        else if(s.equalsIgnoreCase("else"))
+        {
+            return "}\nelse\n{";
+        }
+
+        return "uncaught error";
+    }
+    String comparison(String s)
+    {
+
+        if(s.equalsIgnoreCase("<"))
+        {
+            return " < ";
+        }
+        else if(s.equalsIgnoreCase("<="))
+        {
+            return " <= ";
+        }
+        else if(s.equalsIgnoreCase(">"))
+        {
+            return " > ";
+        }
+        else if(s.equalsIgnoreCase(">="))
+        {
+            return " >= ";
+        }
+        else if(s.equalsIgnoreCase("=="))
+        {
+            return " == ";
+        }
+        else if(s.equalsIgnoreCase("/="))
+        {
+            return " != ";
+        }
+        
+
+        return "uncaught error";
+    }
 }
