ed678a750cd1ea18009217469d9c7fe7dce71df4
HWorrell <31424637+HWorrell@users.noreply.github.com>
2017-09-24 16:11:46 -0400

    Save the type assigned
    This change pushes the type of each member into the Arraylist for that member

--- src/com/company/AssignStatement.java
+++ src/com/company/AssignStatement.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.LinkedList;
 import java.util.StringJoiner;
 import java.util.*;
@@ -7,13 +8,14 @@ public class AssignStatement {
 
     }
 
-    public String integer(String[] s, int index) {
+    public String integer(String[] s, int index, ArrayList<String> integer) {
         StringBuilder build = new StringBuilder();
         build.ensureCapacity(100);
-       // build.append("int ");
+        // build.append("int ");
         for (int i = index; i < s.length; i++) {
             if (!(s[i].equalsIgnoreCase("::") || s[i].equalsIgnoreCase("integer") || s[i].equalsIgnoreCase(",") || s[i].equalsIgnoreCase("=") || s[i].equalsIgnoreCase("0"))) {
                 build.append("int "+s[i].replace(",","") + " = 0;\n");
+                integer.add(s[i].replace(",",""));
             }
         }
         //build.append(";");
@@ -22,26 +24,29 @@ public class AssignStatement {
         return build.toString();
     }
 
-    public String real(String[] s, int index) {
+    public String real(String[] s, int index,  ArrayList<String> real) {
         StringBuilder build = new StringBuilder();
         build.ensureCapacity(100);
         build.append("double ");
         for (int i = index; i < s.length; i++) {
             if (!(s[i].equalsIgnoreCase("::") || s[i].equalsIgnoreCase("double"))) {
                 build.append(s[i] + " ");
+                real.add(s[i].replace(",",""));
             }
         }
         build.append(";");
 
         return build.toString();
     }
-    public String bool(String[] s, int index) {
+    public String bool(String[] s, int index,  ArrayList<String> logical) {
         StringBuilder build = new StringBuilder();
         build.ensureCapacity(100);
         build.append("boolean ");
         for (int i = index; i < s.length; i++) {
             if (!(s[i].equalsIgnoreCase("::") || s[i].equalsIgnoreCase("logical"))) {
                 build.append(s[i] + " ");
+
+                logical.add(s[i].replace(",",""));
             }
         }
         build.append(";");
@@ -51,7 +56,7 @@ public class AssignStatement {
         return build.toString();
     }
 
-    public String character(String[] s, int index) {
+    public String character(String[] s, int index,  ArrayList<String> character) {
         StringBuilder build = new StringBuilder();
         //System.out.println("Hi");
         build.ensureCapacity(100);
@@ -68,6 +73,7 @@ public class AssignStatement {
                 }
             }
         }
+        character.add(build.toString());
         build.append(";");
         return build.toString();
     }
