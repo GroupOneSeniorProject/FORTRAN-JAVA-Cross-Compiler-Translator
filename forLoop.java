96082a20d1e757315e35d28986c56e5bc5a16a13
HWorrell <31424637+HWorrell@users.noreply.github.com>
2017-11-06 18:46:22 -0500

    Update forLoop.java

--- forLoop.java
+++ forLoop.java
@@ -2,17 +2,32 @@ import java.util.LinkedList;
 
 public class forLoop {
 
-/*
-* integer  :: i
-  		do  i=0,20
-  			print  *,i
-  		end  do
-*/
+    String whileLoop(String[] s, int index)
+    {
+        functions fun = new functions();
+        StringBuilder builder = new StringBuilder();
+        String whileParse = s[index].substring(0, 5);
+        builder.append("while(");
+        builder.append(s[index].substring(6, s[index].length()));
+
+        builder.append(fun.logical(s[index + 1]));
+        builder.append(s[index + 2]);
+        builder.append("{");
+
+        return builder.toString();
+    }
+
+    /*
+    * integer  :: i
+              do  i=0,20
+                  print  *,i
+              end  do
+    */
     String forloop(String[] s){
         StringBuilder builder = new StringBuilder();
-    if (s[1].equalsIgnoreCase("DO")) // interprets the lines for DO call in function
-    {
-        for (int i = 0; i < s.length; i++) {
+        if (s[1].equalsIgnoreCase("DO")) // interprets the lines for DO call in function
+        {
+            for (int i = 0; i < s.length; i++) {
 
 
                 String next = s[i];
