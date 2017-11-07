6ca2ccae5148a08430cf6679e72f4da598d577f7
jmonson2 <johnathonmonson@gmail.com>
2017-11-07 12:21:20 -0500

    Updated F95

---
+++ src/com/company/thirdDeliverable.f95
@@ -0,0 +1,54 @@
+program thirdDeliverable
+
+        implicit none
+        
+        integer, dimension(5) :: x
+
+        integer :: y
+        
+        integer :: i
+        
+        x(1) = 1
+
+        x(2) = 2
+
+        x(3) = 3
+
+        x(4) = 4
+
+        x(5) = 5
+
+        y = 5
+
+        i = 1
+        
+        do while(i <= 5 )
+                
+        print *, x(i)
+        
+        i = i + 1
+        
+        end do
+
+        call scalarmult(x, y)
+
+        i = 1
+
+        do while(i <= 5)
+                
+        print *, x(i)
+        
+        i = i + 1
+        
+        end do
+        
+end program thirdDeliverable
+
+subroutine scalarmult(x, y)
+
+        integer, intent(inout) :: x(5)
+
+        integer, intent(in) :: y
+        x = x * y
+
+end subroutine
