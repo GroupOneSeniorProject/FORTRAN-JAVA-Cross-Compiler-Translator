ad588167e2f16150ae8d28d0fe377eb56277ceb4
HWorrell <31424637+HWorrell@users.noreply.github.com>
2017-11-28 17:02:04 -0500

    Add files via upload

--- thirdDeliverable.f95
+++ thirdDeliverable.f95
@@ -1,54 +1,94 @@
+SUBROUTINE incrementOne(byOne)
+
+    IMPLICIT NONE
+
+    INTEGER, INTENT(INOUT) :: byOne
+
+    byOne = byOne + 1
+
+END SUBROUTINE incrementOne
+
+
 program thirdDeliverable
 
         implicit none
         
-        integer, dimension(5) :: x
+        integer, dimension(5) :: xVar
 
-        integer :: y
-        
+        integer :: yVar, byOne, byTwo
+
         integer :: i
-        
-        x(1) = 1
 
-        x(2) = 2
+        byOne = 10
 
-        x(3) = 3
+        print *, "byOne equals ", byOne
 
-        x(4) = 4
+        CALL incrementOne(byOne)
 
-        x(5) = 5
+        print *, "byOne equals ", byOne
 
-        y = 5
+        byTwo = 10
+
+        print *, "byTwo equals ", byTwo
+
+        byTwo = incrementTwo(byTwo)
+
+        print *, "byTwo equals ", byTwo
+
+        xVar(1) = 1
+
+        xVar(2) = 2
+
+        xVar(3) = 3
+
+        xVar(4) = 4
+
+        xVar(5) = 5
+
+        yVar = 5
 
         i = 1
-        
+
         do while(i <= 5 )
-                
-        print *, x(i)
-        
+
+        print *, xVar(i)
+
         i = i + 1
-        
+
         end do
 
-        call scalarmult(x, y)
+        call scalarmult(xVar, yVar)
 
         i = 1
 
         do while(i <= 5 )
-                
-        print *, x(i)
-        
+
+        print *, xVar(i)
+
         i = i + 1
-        
+
         end do
 
-        
+        CONTAINS
+
+            INTEGER FUNCTION incrementTwo(byTwo)
+
+                IMPLICIT NONE
+
+                INTEGER, INTENT(IN) :: byTwo
+
+                incrementTwo = byTwo + 2
+
+                RETURN
+
+            END FUNCTION incrementTwo
+
 end program thirdDeliverable
 
-subroutine scalarmult(x, y)
+subroutine scalarmult(xVar, yVar)
 
-        integer, intent(inout) :: x(5), y
+        integer, intent(inout) :: xVar(5), yVar
 
-        x = x * y
+        xVar = xVar * yVar
 
 end subroutine scalarmult
